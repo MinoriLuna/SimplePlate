718d4ae9a78ce9b85e48a54e7351d62419f4a9f8
MinoriLuna <82196256+MinoriLuna@users.noreply.github.com>
2026-03-30 11:41:32 +0800

    Double Check on Desktop.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/header";
+import Header from "../components/Header";
 import Footer from "../components/footer"; 
 
 const geistSans = Geist({
